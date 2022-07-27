e03ff7a10635a34a16680db21189523e4595fff1
cj <cj@ss.com>
2022-07-27 14:39:18 +0800

    0.0.1

--- cj-life-auth-starter/src/main/java/cj/life/auth/config/ServerSecurityWorkbin.java
+++ cj-life-auth-starter/src/main/java/cj/life/auth/config/ServerSecurityWorkbin.java
@@ -1,14 +1,14 @@
 package cj.life.auth.config;
 
-import cj.life.ability.oauth2.annotation.EnableOAuth2Server;
+import cj.life.ability.oauth2.annotation.EnableCjOAuth2Server;
 import cj.life.ability.oauth2.config.SecurityWorkbin;
-import cj.life.ability.redis.annotation.EnableRedis;
+import cj.life.ability.redis.annotation.EnableCjRedis;
 import cj.life.auth.workbin.ExampleClientDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 
-@EnableRedis
-@EnableOAuth2Server
+@EnableCjRedis
+@EnableCjOAuth2Server
 @Configuration
 public class ServerSecurityWorkbin extends SecurityWorkbin {
     @Override
